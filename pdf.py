a99f2bf5cdcbe4a9f27759bcc71e5e53f1e9bc54
Robert Shi <33047483+Shihanlin@users.noreply.github.com>
2018-12-13 17:23:15 +0800

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -2,15 +2,15 @@
 #coding=utf-8
 from reportlab.lib.styles import getSampleStyleSheet,ParagraphStyle
 from reportlab.lib.pagesizes import letter,A4
-from reportlab.platypus import Paragraph, BaseDocTemplate,PageTemplate,Frame
+from reportlab.platypus import Paragraph, BaseDocTemplate,PageTemplate,Frame,Table,TableStyle
 from reportlab.lib.units import cm
 from reportlab.pdfbase import pdfmetrics,ttfonts
-from reportlab.lib import fonts
+from reportlab.lib import fonts,colors
 
 
 class DOC():
 	def __init__(self,setting):
-		pdfmetrics.registerFont(ttfonts.TTFont('song', 'C:/Windows/Fonts/simsun.ttc'))
+		pdfmetrics.registerFont(ttfonts.TTFont('song', '/home/python/Database_To_PDF/simsun.ttc'))
 		self.filename='report.pdf'
 		self.pagesize=A4
 		self.cm=cm
@@ -58,3 +58,24 @@ class DOC():
 		canvas.line(self.doc.leftMargin,self.doc.bottomMargin+self.doc.height + 4.5*self.cm, self.doc.leftMargin+self.doc.width,\
 		self.doc.bottomMargin+self.doc.height + 4.5*self.cm) 
 		canvas.restoreState()
+
+	def generate_data_table(self,tb_data):
+		
+		#表格数据：用法详见reportlab-userguide.pdf中chapter 7 Table
+		
+		#创建表格对象，并设定各列宽度
+		tbs_table = Table(tb_data, colWidths=[70,60,60,60,60,60])
+		#添加表格样式
+		tbs_table.setStyle(TableStyle([
+		('FONTNAME',(0,0),(-1,-1),'song'),#字体
+		('FONTSIZE',(0,0),(-1,-1),6),#字体大小
+		#('SPAN',(0,0),(3,0)),#合并第一行前三列
+		('BACKGROUND',(0,0),(-1,0), colors.lightskyblue),#设置第一行背景颜色
+		#('SPAN',(-1,0),(-2,0)), #合并第一行后两列
+		('ALIGN',(-1,0),(-2,0),'RIGHT'),#对齐
+		('VALIGN',(-1,0),(-2,0),'MIDDLE'),  #对齐
+		('LINEBEFORE',(0,0),(0,-1),0.1,colors.grey),#设置表格左边线颜色为灰色，线宽为0.1
+		('TEXTCOLOR',(0,1),(-2,-1),colors.royalblue),#设置表格内文字颜色
+		('GRID',(0,0),(-1,-1),0.5,colors.red),#设置表格框线为红色，线宽为0.5
+		]))
+		return tbs_table
